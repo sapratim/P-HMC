94e698fa5e899d26a8f8b5dc1b2656888ce69353
Apratim Shukla <s.apratim@gmail.com>
2025-02-15 00:53:59 +0530

    Update

--- Image_deconv_Cpp/image_deconv_run.R
+++ Image_deconv_Cpp/image_deconv_run.R
@@ -3,18 +3,21 @@
 ###########################  Problem run  ##############################
 ########################################################################
 
-library(magick)
+#library(magick)
 library(fasta)
 library(wavelets)
 library(waveslim)
 source("image_deconv_functions.R")
+load("pixel_mat.Rdata")
 
 ################  Define global variables here ################
 
-img_input <- image_read("Boat_gray.png") 
-img <- image_resize(img_input, "16x16")
-true_pixel_vec <- as.matrix(as.integer(image_data(img)))
-dimen <- sqrt(nrow(true_pixel_vec))
+# img_input <- image_read("Boat_gray.png") 
+# img <- image_resize(img_input, "16x16")
+# pixel_mat <- as.matrix(as.integer(image_data(img)))
+# save(pixel_mat, file = "pixel_mat.Rdata")
+true_pixel_vec <- c(pixel_mat)
+dimen <- sqrt(length(true_pixel_vec))
 H <- blur_func(blur_size = 5)    ######  Blur matrix
 Psi <- haar_matrix(8)   #####  Haar frame
 y <- convolve_image(true_pixel_vec, dimen, dimen, H)
@@ -40,26 +43,30 @@ freq_mode <- fasta(f_freq, gradf_freq, g, proxg, y, tau1 = 5)
 ########################  Chaari/Durmus runs  ########################
 
 system.time(pxhmc_chaari_run <- pxhmc_chaari(y = y, lambda <- 10, f, gradf, g, proxg, 
-                           iter = 1e3, eps_hmc <- 0.04, L = 10, freq_mode$x,freq_mode$x, 3))
+                           iter = 1e2, eps_hmc <- 0.04, L = 10, freq_mode$x,freq_mode$x, 3))
 
-system.time(pxhmc_dur_run <- pxhmc_dur(y, lambda = 10, iter = 1e4, eps_hmc = 0.03, L = 10,
+system.time(pxhmc_dur_run <- pxhmc_dur(y, lambda = 10, iter = 1e2, eps_hmc = 0.03, L = 10,
                            start = freq_mode$x))
+
+output <- list(pxhmc_chaari_run[[1]], pxhmc_dur_run[[1]])
+
+save(output, file = "samples.Rdata")
 
 ###############################  Mode matching  ###############################
 ###############################   Validation    ###############################
 ##############################  Run sequentially ##############################
 
-######  Chaari ######
-
-i <- 1
-plot(density(pxhmc_chaari_run[[1]][,i]), type = 'l')
-abline(v = freq_mode$x[i], col = "red")
-i <- i+1
-
-######  Durmus ###### 
-
-i <- 1
-plot(density(pxhmc_dur_run[[1]][,i]), type = 'l')
-abline(v = freq_mode$x[i], col = "red")
-i <- i+1
-
+# ######  Chaari ######
+# 
+# i <- 1
+# plot(density(pxhmc_chaari_run[[1]][,i]), type = 'l')
+# abline(v = freq_mode$x[i], col = "red")
+# i <- i+1
+# 
+# ######  Durmus ###### 
+# 
+# i <- 1
+# plot(density(pxhmc_dur_run[[1]][,i]), type = 'l')
+# abline(v = freq_mode$x[i], col = "red")
+# i <- i+1
+# 
