1c9439c95131f0765e23e3e9c9551542affcf646
Dootika Vats <dootika1990@gmail.com>
2025-07-02 08:55:49 +0530

    push

--- Examples/NuclearNorm/single_output.R
+++ Examples/NuclearNorm/single_output.R
@@ -1,53 +1,24 @@
-
 load("single_run.Rdata")
+library(mcmcse)
 
 durhmc <- output_single_hmc[[1]]
 pxhmc <- output_single_hmc[[2]]
 
-library(mcmcse)
+
 ess_dur <- ess(durhmc)
 ess_px <- ess(pxhmc)
 
-hist(ess_dur, col = adjustcolor("purple", alpha.f = .3), xlim = c(0, max(ess_dur)))
-hist(ess_px, add = TRUE, col = adjustcolor("red", alpha.f = .3))
+pdf("nn_hist_ess.pdf", height = 5, width = 7)
+hist(ess_dur, col = adjustcolor("purple", alpha.f = .5), xlim = c(0, max(ess_dur)),
+     xlab = "Effective sample size for all components", main = "")
+hist(ess_px, add = TRUE, col = adjustcolor("orange", alpha.f = .7))
+legend("top", fill = c(adjustcolor("purple", .5), adjustcolor("orange", .7)), 
+       legend = c("Partial Proximal", "Full Proximal"))
+dev.off()
 
-plot(ess_dur, ess_px, ylim = c(0, max(ess_dur)), xlim = c(0, max(ess_dur)))
-abline(a = 0, b = 1)
-
-
-
-rand <- 1:length(y) #sample(c(1:length(y)), subset)
-dim <- length(y)
-lag.max <- 100
-acf_dur_hmc <- acf(output_single_hmc[[1]][,rand[1]], plot = FALSE, lag.max = lag.max)$acf
-acf_pxhmc <- acf(output_single_hmc[[2]][,rand[1]], plot = FALSE, lag.max = lag.max)$acf
-
-diff.acf <- matrix(0, ncol = dim, nrow = lag.max + 1)
-diff.acf[,1] <- acf_is_hmc - acf_pxhmc
-
-
-acf(result_durhmc[[1]][, 2])
-acf(result_pxhmc[[1]][, 2])
-
-for (i in 1:dim) 
-{
-  if(i %% 1000 == 0) print(i)
-  acf_dur_hmc <- acf(output_single_hmc[[1]][,i], plot = FALSE, lag.max = lag.max)$acf
-  acf_pxhmc <- acf(output_single_hmc[[2]][,i], plot = FALSE, lag.max = lag.max)$acf
-  diff.acf[,i] <- acf_dur_hmc - acf_pxhmc
-}
-
-
-# Make boxplot of ACFs
-boxplot(t(diff.acf),
-        xlab = "Lags", col = "pink",
-        ylab = "Difference in ACFs of HMCs",ylim = range(diff.acf),
-        names = 0:lag.max, show.names = TRUE, range = 3)
-#dev.off()
-
-
-
-# save(output_single_mala, file = "output_single_chain_mala.Rdata")
-# save(output_single_bark, file = "output_single_chain_bark.Rdata")
-# save(output_single_hmc, file = "output_single_chain_hmc.Rdata")
-
+# 
+# 
+# plot(ess_dur, ess_px, ylim = c(0, max(ess_dur)), xlim = c(0, max(ess_dur)))
+# abline(a = 0, b = 1)
+# 
+# 
